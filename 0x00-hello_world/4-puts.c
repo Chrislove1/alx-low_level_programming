831bac7140ed2d1d7853c11f309082992a60a2ab
Chrislove1 <simoncpeter04@gmail.com>
2022-03-10 19:57:59 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase programming is like building a multiligual puzzle
+ * main - prints the phrase "programming is like building a multiligual puzzle
  * return: 0 if exited properly, non-zero otherwise
  */
-int main (void)
+int main(void)
 {
-	puts("programming is like building a multiligual puzzle,\n");
-	return (0);
+    puts("programming is like building a multiligual puzzle,\n");
+    return(0);
 }
