572e96a6462038a06f3d55f6999d94b0a8e08191
Chrislove1 <simoncpeter04@gmail.com>
2022-03-10 19:29:22 -0800

    PUTS,C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multiligual puzzle
+ * main - prints the phrase programming is like building a multiligual puzzle
  * return: 0 if exited properly, non-zero otherwise
  */
 int main (void)
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,5 @@
 # 0x00-hello_world 
 # 0x00-hello_world 
 # 0x00-hello_world 
+# 0x00-hello_world 
+# 0x00-hello_world 
