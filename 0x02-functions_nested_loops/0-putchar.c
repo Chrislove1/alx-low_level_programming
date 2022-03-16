7b952ce2213e3ad700015f58d130673c89ba6028
Chrislove1 <simoncpeter04@gmail.com>
2022-03-16 15:14:28 +0100

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+int main (void)
+{
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar;
+	_putchar('\n');
+
+
+	return (0);
