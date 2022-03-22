2dbb9c42e5b236dc2b51529c6e380346e560925a
Chrislove1 <simoncpeter04@gmail.com>
2022-03-23 00:25:44 +0100

    battery st

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+include "main.h"
+
+_putchar(char c);
+void rest_to_98(int *n);
+void saw_int(int *a, int *b);
+int _strlne(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
