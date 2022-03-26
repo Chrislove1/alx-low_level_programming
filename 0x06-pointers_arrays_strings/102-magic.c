24f0f708203cab2ac0dda828c4557955d06b6e6d
Chrislove1 <simoncpeter04@gmail.com>
2022-03-26 04:53:38 +0100

    magic hand..c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,8 +3,10 @@
 * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-   int main (void)
-*(p+5) = 98;
+   
+int main (void)
+
+  *(p+5) = 98;
   *(p+5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
