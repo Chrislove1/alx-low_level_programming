6dd217fd71386bbfd7149dae7141e330624c6660
Chrislove1 <simoncpeter04@gmail.com>
2022-03-31 01:42:36 +0100

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,8 +10,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int wildcmp(char *s1, char *s2);
+int is_prime_mumber(int n);
 int _sqrt(int n, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif /* MAIN_H */
