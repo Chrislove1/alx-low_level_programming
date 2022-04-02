00e0295901c5fcd2f864e3de24bd8ab3d6df6f44
Chrislove1 <simoncpeter04@gmail.com>
2022-04-02 08:09:46 +0100

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -6,13 +6,13 @@
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int c);
-int _upper(int c);
+int _abs(int n);
+int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-char *_atoi(char *s);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
